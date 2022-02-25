b5f0395b5d4608ec10be389d6ec83ab02e70d434
alex-pythonista <indrajit.alex@gmail.com>
2022-02-25 22:41:29 +0600

    update today

---
+++ express-demo/index.js
@@ -0,0 +1,36 @@
+const express = require('express');
+app = express();
+
+app.use(express.json());
+
+const courses = [
+    { id: 1, name: 'course1' },
+    { id: 2, name: 'course2' },
+    { id: 3, name: 'course3' },
+];
+
+app.get('/', (req, res) => {
+    res.send('Hello World');
+});
+
+app.get('/api/courses', (req, res) => {
+    res.send(courses);
+});
+
+app.get('/api/courses/:id', (req, res) => {
+    let course = courses.find(c => c.id === parseInt(req.params.id));
+    if (!course) res.status(404).send('The course with the given ID was not found.');
+    else res.send(course);
+});
+
+app.post('/api/courses', (req, res) => {
+    let course = {
+        id: courses.length + 1,
+        name: req.body.name,
+    }
+    courses.push(course);
+    res.send(course);
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Listening on port ${port}...`));
